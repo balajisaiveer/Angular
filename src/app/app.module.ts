da364313e85a08e7f71dae620ee1cf05cc62304b
balajisaiveer <puppalabalaji@gmail.com>
2019-07-03 14:07:55 +0530

    added general usages of ngFor ngIf

---
+++ src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { OrderModule } from 'ngx-order-pipe';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { FormsModule } from '@angular/forms';
+
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule ,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatChipsModule ,
+        MatIconModule ,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatExpansionModule ,
+        MatGridListModule ,
+        MatCheckboxModule,
+        MatMenuModule,
+        MatTooltipModule
+} from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent   
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    OrderModule,
+    FilterPipeModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTooltipModule
+
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
