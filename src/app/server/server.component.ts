fd718c317cc22a291e797772b8af4822f5af6cb5
Puppala <Balaji.Puppala@cotiviti.com>
2019-07-11 16:53:22 +0530

    fixing lint errors

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,55 +7,54 @@ import { ThrowStmt } from '@angular/compiler';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  newServerStatus = 'No new server';
-  allowNewServer = false;
-  servername='TestServer';
-  newservercreated= false;
-  serverstatus: string = 'offfline';
-  servernames = ['server1','server2']
-  log=[]
-  isblue = true;
-  showSecret = false;
-  truth="";
   constructor() {
-    this.serverstatus = Math.random() > 0.5 ?'online' : 'offline';
+    this.serverstatus = Math.random() > 0.5 ? 'online' : 'offline';
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
    }
+  newServerStatus = 'No new server';
+  allowNewServer = false;
+  servername = 'TestServer';
+  newservercreated = false;
+  serverstatus = 'offfline';
+  servernames = ['server1', 'server2'];
+  log = [];
+  isblue = true;
+  showSecret = false;
+  truth = '';
+  serverid = 10;
 
   ngOnInit() {
   }
-  serverid: number = 10;
 
 
-  onToogleDeatils(){
+  onToogleDeatils() {
     this.showSecret = !this.showSecret;
     this.log.push(new Date());
   }
-  ontrue(){
-    this.truth="false";
+  ontrue() {
+    this.truth = 'false';
     return this.truth;
   }
-  onServerCreated(){
-    this.newservercreated= true;
+  onServerCreated() {
+    this.newservercreated = true;
     this.servernames.push(this.servername);
     this.newServerStatus = 'New server is created' + this.servername;
   }
-  getServerStatus(){
+  getServerStatus() {
     return this.serverstatus;
   }
-  getcolor(){
-    if(this.serverstatus=="offline"){
-      return "red";
+  getcolor() {
+    if (this.serverstatus === 'offline') {
+      return 'red';
 
-    }
-    else{
-      return "green";
+    } else {
+      return 'green';
     }
   }
-  onEnterEvent(event: any){
+  onEnterEvent(event: any) {
     this.servername =   (<HTMLInputElement>event.target).value;
   }
-  
+
 }
